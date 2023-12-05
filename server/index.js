12ee61a93e5437bf836c44bc05c653be6378e15a
eskander-taher <eskander.alshaibany@gmail.com>
2023-12-05 05:10:54 +0500

    fix nodemon and dotenv

--- server/index.js
+++ server/index.js
@@ -1,3 +1,4 @@
+const dotenv = require("dotenv").config();
 const createApp = require("./createApp");
 
 const PORT = process.env.PORT | 5000;
@@ -5,4 +6,3 @@ const PORT = process.env.PORT | 5000;
 const server = createApp();
 
 server.listen(PORT, () => console.log(`Running on port http://localhost:${PORT}`));
-
