8e6c530e1a2ace45c808ce61d1cb52e94f94a4cb
eskander-taher <eskander.alshaibany@gmail.com>
2023-12-12 02:08:53 +0500

    add github actions

--- deployment/deployment_steps.md
+++ deployment/deployment_steps.md
@@ -4,5 +4,10 @@
 4. install node & npm
 5. install nginx
 6. install ufw if needed, setup firewall "ufw allow http, ufw allow "Nginx HTTP", ufw allow ssh"
-7.create linux user "adduser username
-
+7. create linux user "adduser username"
+8. give user sudo prevleges "sudo usermod -aG sudo userName"
+9. go to github project repo > settings > actions > add runner and follow instructions
+10. get user permissions to do whatever it need in actions runner folder "sudo chmod -R 777 /var/www/actions-runner"
+11. run config command from github instructions
+12. anstall and run ./svc.sh "sudo ./svc.sh install sudo ./svc.sh start"
+acs ALL=(ALL) NOPASSWD: /usr/sbin/service nginx start,/usr/sbin/service nginx stop,/usr/sbin/service nginx restart
--- server/createApp.js
+++ server/createApp.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const cors = require("cors");
 const swagger = require("./docs/swagger");
+const path = require("path")
 
 function createApp() {
 	// Express app setup: Enable CORS, parse requests
@@ -8,11 +9,12 @@ function createApp() {
 	app.use(cors());
 	app.use(express.json());
 	app.use(express.urlencoded({ extended: false }));
+	app.use(express.static(path.join(__dirname, '../client/build')))
 
 	// Resources routes
-	app.use("/api/users", require("./api/routes/userRoutes"));
-	app.use("/api/buildings", require("./api/routes/buildingRoutes"));
-	app.use("/api/access", require("./api/routes/accessRoutes"));
+	app.use("/users", require("./api/routes/userRoutes"));
+	app.use("/buildings", require("./api/routes/buildingRoutes"));
+	app.use("/access", require("./api/routes/accessRoutes"));
 
 	swagger(app);
 	return app;
