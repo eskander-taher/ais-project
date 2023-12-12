4aa71f303e322166b1d268bc09e16fb073136374
eskander-taher <eskander.alshaibany@gmail.com>
2023-12-12 08:39:45 +0500

    add user

--- client/src/pages/users/Users.jsx
+++ client/src/pages/users/Users.jsx
@@ -1,88 +1,114 @@
-import { useState, useEffect } from "react";
-// import UserList from "./UserList";
+import React, { useState, useEffect } from "react";
 import AddItemModal from "../../components/AddItemModal";
 import useApi from "../../utils/useApi";
 import { USERS_API_URL } from "../../utils/constants";
 
 function UserList({ data, loading, error, onDeleteItem }) {
-	if (loading) {
-		return <p>Loading...</p>;
-	}
+  if (loading) {
+    return <p>Loading...</p>;
+  }
 
-	if (error) {
-		return <p>Error: {error.message}</p>;
-	}
-	return (
-		<table className="table">
-			<thead>
-				<tr>
-					<th>User ID</th>
-					<th>User Name</th>
-					<th>CreatedAt</th>
-				</tr>
-			</thead>
-			<tbody>
-				{data.map((user) => (
-					<tr key={user.id}>
-						<td>{user.id}</td>
-						<td>{user.name}</td>
-						<td>{user.createdAt}</td>
-						<td>
-							<button className="delete-btn" onClick={() => onDeleteItem(user.id)}>Delete</button>
-						</td>
-					</tr>
-				))}
-			</tbody>
-		</table>
-	);
+  if (error) {
+    return <p>Error: {error.message}</p>;
+  }
+
+  return (
+    <table className="table">
+      <thead>
+        <tr>
+          <th>User ID</th>
+          <th>User Name</th>
+          <th>CreatedAt</th>
+          <th>Action</th>
+        </tr>
+      </thead>
+      <tbody>
+        {data.map((user) => (
+          <tr key={user.id}>
+            <td>{user.id}</td>
+            <td>{user.name}</td>
+            <td>{user.createdAt}</td>
+            <td>
+              <button
+                className="delete-btn"
+                onClick={() => onDeleteItem(user.id)}
+              >
+                Delete
+              </button>
+            </td>
+          </tr>
+        ))}
+      </tbody>
+    </table>
+  );
 }
 
 export default function Users() {
-	const { data, error, loading, postData, deleteData, putData } = useApi(USERS_API_URL);
+  const { data, error, loading, postData, deleteData } = useApi(USERS_API_URL);
 
-	const [isAddItemModalOpen, setAddItemModalOpen] = useState(false);
+  const [isAddItemModalOpen, setAddItemModalOpen] = useState(false);
+  const [confirmDelete, setConfirmDelete] = useState(null);
 
-	const handleAddItemClick = () => {
-		setAddItemModalOpen(true);
-	};
+  const handleAddItemClick = () => {
+    setAddItemModalOpen(true);
+  };
 
-	const handleCloseModal = () => {
-		setAddItemModalOpen(false);
-	};
+  const handleCloseModal = () => {
+    setAddItemModalOpen(false);
+  };
 
-	const handleAddItem = (itemData) => {
-		postData(itemData);
-		handleCloseModal();
-	};
+  const handleAddItem = (userData) => {
+    // Validate input and then add the user
+    if (userData.name.trim()) {
+      postData(userData);
+      handleCloseModal();
+    }
+  };
 
-	const handleDeleteItem = (userId) => {
-		// Call the deleteData function with the user ID to delete
-		deleteData(userId);
-	};
+  const handleDeleteItem = (userId) => {
+    // Show confirmation dialog before deletion
+    setConfirmDelete(userId);
+  };
 
-	useEffect(() => {
-		// You may want to refetch data after a successful deletion
-		// to update the user list
-	}, [data]);
+  const confirmDeleteAction = () => {
+    // Actual deletion after confirmation
+    deleteData(confirmDelete);
+    setConfirmDelete(null);
+  };
 
-	console.log(data);
+  const cancelDeleteAction = () => {
+    // Cancel deletion
+    setConfirmDelete(null);
+  };
 
-	return (
-		<div>
-			<button className="add-button" onClick={handleAddItemClick}>
-				Add user
-			</button>
-			<AddItemModal
-				isOpen={isAddItemModalOpen}
-				onClose={handleCloseModal}
-				onAddItem={handleAddItem}
-			/>
-			<UserList
-				data={data}
-				loading={loading}
-				error={error}
-				onDeleteItem={handleDeleteItem} // Pass the delete function to UserList
-			/>
-		</div>
-	);
+  return (
+    <div>
+      <button className="add-button" onClick={handleAddItemClick}>
+        {loading ? "Loading..." : "Add User"}
+      </button>
+      <AddItemModal
+        isOpen={isAddItemModalOpen}
+        onClose={handleCloseModal}
+        onAddItem={handleAddItem}
+        inputPlaceholder="User Name"
+        inputLabel="Enter User Name:"
+      />
+      {error && <p>Error: {error.message}</p>}
+      <UserList
+        data={data}
+        loading={loading}
+        error={error}
+        onDeleteItem={handleDeleteItem}
+      />
+      {confirmDelete && (
+        <div className="confirmation-dialog">
+          <p>Are you sure you want to delete this user?</p>
+          <button className="delete-btn" onClick={confirmDeleteAction}>
+            Yes
+          </button>
+          <button onClick={cancelDeleteAction}>No</button>
+        </div>
+      )}
+    </div>
+  );
 }
--- client/src/utils/constants.js
+++ client/src/utils/constants.js
@@ -1,3 +1,3 @@
-export const USERS_API_URL = '/users';
-export const BUILDINGS_API_URL = '/buildings';
-export const ACCESSLOG_API_URL = '/access';
+export const USERS_API_URL = 'http://localhost:5000/users';
+export const BUILDINGS_API_URL = 'http://localhost:5000/buildings';
+export const ACCESSLOG_API_URL = 'http://localhost:5000/buildings';
