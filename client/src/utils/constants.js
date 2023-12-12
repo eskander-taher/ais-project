5fb6241468f0864c8cccb33fede6fcae7476a33b
eskander-taher <eskander.alshaibany@gmail.com>
2023-12-12 08:17:45 +0500

    building

--- client/src/utils/constants.js
+++ client/src/utils/constants.js
@@ -1,3 +1,3 @@
-export const USERS_API_URL = 'http://144.126.192.45:5000/users';
-export const BUILDINGS_API_URL = 'http://144.126.192.45:5000/buildings';
-export const ACCESSLOG_API_URL = 'http://144.126.192.45:5000/access';
+export const USERS_API_URL = '/users';
+export const BUILDINGS_API_URL = '/buildings';
+export const ACCESSLOG_API_URL = '/access';
